27885d09bb397f817fecf0ae5943ca76d0e31380
jprd96 <jktpark1@gmail.com>
2022-12-06 15:38:58 -0800

    better solution to 4

--- 004.py
+++ 004.py
@@ -8,18 +8,15 @@ import time
 start_time = time.time()
 
 def solve():
-    n = 999*999
-    palindromes = []
-    res = []
-    for n in range(n,-1,-1):
-        strn = str(n)
-        if strn == strn[::-1]:
-            palindromes.append(n)
-    for i in reversed(range(100,999)):
-        for j in reversed(range(100, 999)):
-            if i*j in palindromes:
-                res.append(i*j)
-    return max(res)
+    res = 0
+    for i in range(100, 999):
+        for j in range(100, 999):
+            x = i*j
+            if x > res:
+                s = str(x)
+                if s == s[::-1]:
+                    res = x            
+    return res
            
 if __name__ == '__main__':
     print(solve())
